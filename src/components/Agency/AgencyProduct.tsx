1a0af0fc96a76807895ac65da730590bf3d1840c
Alan Oliveira <alancesar2007@gmail.com>
2021-08-08 00:33:20 -0300

    adding influencer and agency images

--- src/components/Agency/AgencyProduct.tsx
+++ src/components/Agency/AgencyProduct.tsx
@@ -21,7 +21,11 @@ const AgencyProduct = () => {
 			<Image src={"/agency.svg"} alt={""} height={300} width={300} />
 			<h2>O nosso comparador considera uma série de fatores para tomada de decisão, tais como:</h2>
 			<Image src={"/agency_factors.svg"} alt={""} height={300} width={300} />
-			{/* <h1>Planos</h1> */}
+			<Image
+				src={"/agency_acc_compare.svg"} alt={""} height={500} width={500}
+				className={`${styles.compare}`}
+			/>
+			<p>Comparamos múltiplas contas no TikTok, te mostrando quais se sobressaem em determinados aspectos</p>
 			<PricingTag title={"Compare 2 contas"} price={"R$ 25,00"} />
 			<PricingTag title={"Compare 4 contas"} price={"R$ 35,00"} />
 			<PricingTag title={"Compare 10  contas"} price={"R$ 55,00"} />
--- src/components/Influencer/InfluencerProduct/index.tsx
+++ src/components/Influencer/InfluencerProduct/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from 'react';
+import Image from 'next/image';
 
 import styles from './influencerproduct.module.scss';
 
@@ -19,7 +20,16 @@ const InfluencerProduct = () => {
 			ref={containerRef}
 		>
 			<h1>Influencer,</h1>
-			<h2>receba uma consultoria personalizada e totalmente automatizada com a análise do seu perfil</h2>
+			<h2>receba uma <strong>consultoria</strong> personalizada e totalmente <strong>automatizada</strong> com a análise do seu perfil</h2>
+			<section>
+				<img src="inf1.png" alt="" />
+				<p>Coletamos os dados de <strong>todos</strong> os seus vídeos</p>
+				<img src="inf2.png" alt="" />
+				<p>Os dados são processados e analisados</p>
+				<img src="inf3.png" alt="" />
+				<p>Entregamos um relatório completo com as análises e dicas</p>
+			</section>
+			<h3>Plano</h3>
 			<PricingTag
 				title={"Consultoria completa do perfil"}
 				price={"R$ 35,00"}
