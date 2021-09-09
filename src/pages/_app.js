99728b801467683e61a3ce76977b5fdd0b87a77b
Alan Oliveira <alancesar2007@gmail.com>
2021-09-09 11:02:18 -0300

    adding google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,7 @@
-import '../styles/globals.scss'
+import '../styles/globals.scss';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize("UA-207192105-1");
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
