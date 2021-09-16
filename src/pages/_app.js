148aa1d393cab5f7f13e2a2371a38d081075b329
Alan Oliveira <alancesar2007@gmail.com>
2021-09-16 20:07:30 -0300

    changing google analytics setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,33 @@
 import '../styles/globals.scss';
-import ReactGA from 'react-ga';
+import Script from 'next/script';
+// import ReactGA from 'react-ga';
 
-ReactGA.initialize("UA-207192105-1");
+// ReactGA.initialize("UA-207192105-1");
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=UA-207192105-1`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-207192105-1', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
