de6c48c5353a4f924f6bd3dc8803ac40411e26a8
Alan Oliveira <alancesar2007@gmail.com>
2021-09-16 20:25:31 -0300

    adding more details to google analytics

--- src/components/Princing/PrincingTag/index.tsx
+++ src/components/Princing/PrincingTag/index.tsx
@@ -3,12 +3,22 @@ import React from 'react';
 import styles from './princingtag.module.scss';
 import sendMessage from '../../../hooks/useWpp';
 
+import * as gtag from '../../../lib/gtag';
+
 const PrincingTag = ({ title, price }) => {
+    const recordGaEvent = () => {
+        gtag.event({
+            action: 'buy_click',
+            category: 'product',
+            label: title,
+            value: price
+        })
+    }
     return (
         <div className={styles.container}>
             <p>{title}</p>
             <p className={styles.price}>{price}</p>
-            <button onClick={sendMessage} >Contratar</button>
+            <button onClick={() => { recordGaEvent(); sendMessage() }} >Contratar</button>
         </div>
     )
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,33 +1,45 @@
+import { useEffect } from 'react'
 import '../styles/globals.scss';
 import Script from 'next/script';
-// import ReactGA from 'react-ga';
-
-// ReactGA.initialize("UA-207192105-1");
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=UA-207192105-1`}
-      />
-      <Script
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+
+	const router = useRouter()
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			gtag.pageview(url)
+		}
+		router.events.on('routeChangeComplete', handleRouteChange)
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange)
+		}
+	}, [router.events])
+
+	return (
+		<>
+			{/* Global Site Tag (gtag.js) - Google Analytics */}
+			<Script
+				strategy="afterInteractive"
+				src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+			/>
+			<Script
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'UA-207192105-1', {
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
               page_path: window.location.pathname,
             });
           `,
-        }}
-      />
-      <Component {...pageProps} />
-    </>
-  )
+				}}
+			/>
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default MyApp
